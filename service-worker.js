8c9d93a0c9e0c6463f6359bd9d7ea7fc190b9d8d
tyrcab <tyroncabral@gmail.com>
2025-09-17 16:06:34 +1000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,20 @@
-const CACHE_NAME = "shift-app-v1";
+const CACHE_NAME = "dng-shifts-cache-v1";
 const urlsToCache = [
+  "./",
   "./index.html",
   "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  "https://raw.githubusercontent.com/tyrcab/Metro-Shift-Rotation-Times-app/main/icons/icon-192.png",
+  "https://raw.githubusercontent.com/tyrcab/Metro-Shift-Rotation-Times-app/main/icons/icon-512.png"
 ];
 
-self.addEventListener("install", (event) => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
